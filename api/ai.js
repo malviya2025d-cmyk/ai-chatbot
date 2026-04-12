2268fb441fad319c70d82ac397cca15fcb19b284
malviya2025d-cmyk <malviya2025d@gmail.com>
2026-04-13 00:59:56 +0530

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -6,8 +6,10 @@ export default async function handler(req, res) {
   try {
     const { prompt } = req.body;
 
+    const API_KEY = process.env.GEMINI_API_KEY;
+
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=AIzaSyDzSRe6KzwQkVunyePfhICsvPHem_kxuwM",
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
       {
         method: "POST",
         headers: {
@@ -24,8 +26,6 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
-
-    console.log(data); // debugging
 
     const reply =
       data?.candidates?.[0]?.content?.parts?.[0]?.text ||
