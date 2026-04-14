dc26ccf1dc826b59a796149a45efac4fbcad6254
malviya2025d-cmyk <malviya2025d@gmail.com>
2026-04-15 01:14:24 +0530

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -1,9 +1,3 @@
-export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(200).send("Only POST allowed");
-  }
-
-  return res.status(200).json({
-    result: "API working ✅"
-  });
+export default function handler(req, res) {
+  res.status(200).json({ message: "API WORKING ✅" });
 }
