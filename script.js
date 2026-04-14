70da48b2ce78dc5a7356c0993aa6648481c0ceee
malviya2025d-cmyk <malviya2025d@gmail.com>
2026-04-15 01:21:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-async function send() {
+async function sendMessage() {
   const input = document.getElementById("input").value;
 
-  const res = await fetch("/api/ai", {
+  const res = await fetch("https://ai-chatbot-sw21-git-main-malviya2025d-9773s-projects.vercel.app/api/ai", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({ prompt: input })
+    body: JSON.stringify({
+      prompt: input
+    })
   });
 
   const data = await res.json();
 
-  document.getElementById("chatbox").innerHTML += 
-    "<p><b>You:</b> " + input + "</p>" +
-    "<p><b>AI:</b> " + data.result + "</p>";
+  document.getElementById("output").innerText = data.result;
 }
